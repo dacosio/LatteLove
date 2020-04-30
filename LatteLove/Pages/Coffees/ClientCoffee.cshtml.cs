c5b8b1d733dfec658055399882edec6373f0ef43
cosio <ucg.don.cosio@gmail.com>
2020-04-30 09:30:34 +0800

    Edit validation

---
+++ LatteLove/Pages/Coffees/ClientCoffee.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LatteLove.Pages.Coffees
+{
+    public class ClientCoffeeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
